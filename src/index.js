e11bbad5bafa98dfb7a17387201fbed9dff2e4e6
AnaRativa09 <anarativa09@gmail.com>
2021-11-26 19:05:18 -0500

    feat: add multipleCustomHook component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
 
 // import { HookApp } from './HookApp';
 // import { CounterApp } from './components/01-useState/CounterApp';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';/
 // import { SimpleForm } from './components/02-useEffect/SimpleForm';
-import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
+// import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
+
 
 ReactDOM.render(
-    <FormWithCustomHook />,
+    <MultipleCustomHooks />,
   document.getElementById('root')
 );
 
